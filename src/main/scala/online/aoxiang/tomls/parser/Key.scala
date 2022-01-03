0f54833a8b57608fcbc33b18c859233499fbb614
叶子航 <jerryzihye@outlook.com>
2022-01-03 13:50:51 +0800

    Add key parser 添加键解析器

---
+++ src/main/scala/online/aoxiang/tomls/parser/Key.scala
@@ -0,0 +1,19 @@
+package online.aoxiang.tomls.parser
+
+import cats.parse.Parser
+import cats.parse.Rfc5234._
+import Parsers._
+import cats.data.NonEmptyList
+
+object PKey {
+  val parser: Parser[NonEmptyList[String]] = {
+    val quoted_key: Parser[String] = PString.basicStringParser.map(_.value) | PString.literalStringParser.map(_.value)
+    val unquoted_key: Parser[String] = (alpha | digit | Parser.charIn("_-")).rep.map(_.toList.mkString)
+    val simple_key: Parser[String] = quoted_key | unquoted_key
+
+    val dot_sep = Parser.char('.').surroundedBy(ws)
+    val dotted_key: Parser[NonEmptyList[String]] = simple_key.repSep(dot_sep)
+
+    dotted_key.backtrack
+  }
+}
---
+++ src/test/scala/online/aoxiang/tomls/parser/KeyTest.scala
@@ -0,0 +1,45 @@
+package online.aoxiang.tomls.parser
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+import cats.data.NonEmptyList
+
+class KeyTest extends AnyFunSuite with Matchers with ScalaCheckPropertyChecks {
+  test("Parse valid key") {
+    // Cases from official document
+    val bareKeys = Table("bare key", "key", "bare_key", "bare-key", "1234")
+    forAll(bareKeys) { s =>
+      PKey.parser.parseAll(s) should be(Right(NonEmptyList(s, Nil)))
+    }
+
+    val quotedKeys = Table(
+      ("quoted key", "expected"),
+      (raw""""127.0.0.1"""", "127.0.0.1"),
+      (raw""""character encoding"""", "character encoding"),
+      (raw""""ʎǝʞ"""", "ʎǝʞ"),
+      (raw"'key2'", "key2"),
+      (raw"""'quoted "value"'""", "quoted \"value\""),
+      (raw"""""""", ""),
+      ("''", "")
+    )
+    forAll(quotedKeys) { (s, expected) =>
+      PKey.parser.parseAll(s) should be(Right(NonEmptyList(expected, Nil)))
+    }
+
+    val dottedKeys = Table(
+      ("dotted key", "expected"),
+      ("name", NonEmptyList("name", Nil)),
+      ("physical.color", NonEmptyList("physical", List("color"))),
+      ("physical.shape", NonEmptyList("physical", List("shape"))),
+      ("site.\"google.com\"", NonEmptyList("site", List("google.com"))),
+      ("fruit.name", NonEmptyList("fruit", List("name"))),
+      ("fruit. color", NonEmptyList("fruit", List("color"))),
+      ("fruit . flavor", NonEmptyList("fruit", List("flavor"))),
+      ("3.14159", NonEmptyList("3", List("14159")))
+    )
+    forAll(dottedKeys) { (s, expected) =>
+      PKey.parser.parseAll(s) should be(Right(expected))
+    }
+  }
+}
